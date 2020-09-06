048031e784414bfb8373db6fab24cdfa6000cc11
doug147 <douglorenzen@gmail.com>
2020-09-06 17:56:52 -0400

    Create ClientComponentAlwaysNoon.cs

---
+++ ClientComponentAlwaysNoon.cs
@@ -0,0 +1,26 @@
+namespace CryoFall.Noon
+{
+    using AtomicTorch.CBND.CoreMod.Systems.TimeOfDaySystem;
+    using AtomicTorch.CBND.GameApi.Scripting.ClientComponents;
+    public class ClientComponentAlwaysNoon : ClientComponent
+    {
+        private double originalOffset = 0.0;
+        
+        public void Setup(double originalOffset)
+        {
+            this.originalOffset = originalOffset;
+        }
+        public override void Update(double deltaTime)
+        {
+            var currentOffset = TimeOfDaySystem.ServerTimeOfDayOffsetSeconds;
+            var currentTimeOfDayseconds = TimeOfDaySystem.CurrentTimeOfDaySeconds / 60;
+            var newOffset = originalOffset - (currentTimeOfDayseconds - currentOffset);
+            var obj = new object[1] { newOffset };
+            var sut = TimeOfDaySystem.Instance;
+            typeof(TimeOfDaySystem)
+                .GetMethod("ClientRemote_ServerTimeOfDayOffsetUpdate",
+                    System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .Invoke(sut, obj);
+        }
+    }
+}
